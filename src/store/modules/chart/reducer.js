e7b9016fc0ef66fd3f23b5061e491601c7053f52
lucas-silva-dev <lucassilva.work@outlook.com>
2020-08-01 03:52:32 -0300

    add loadings

--- src/store/modules/chart/reducer.js
+++ src/store/modules/chart/reducer.js
@@ -2,16 +2,23 @@ import produce from 'immer';
 
 const INITIAL_STATE = {
   chart: null,
+  loading: false,
 };
 
 export default function chart(state = INITIAL_STATE, action) {
   return produce(state, draft => {
     switch (action.type) {
+      case '@chart/CHART_REQUEST': {
+        draft.loading = true;
+        break;
+      }
       case '@chart/CHART_SUCCESS': {
         draft.chart = action.payload.chart;
+        draft.loading = false;
         break;
       }
       case '@chart/CHART_FAILURE': {
+        draft.loading = false;
         break;
       }
       default:
--- src/store/modules/stock/reducer.js
+++ src/store/modules/stock/reducer.js
@@ -2,16 +2,23 @@ import produce from 'immer';
 
 const INITIAL_STATE = {
   stock: {},
+  loading: false,
 };
 
 export default function stock(state = INITIAL_STATE, action) {
   return produce(state, draft => {
     switch (action.type) {
+      case '@stock/STOCK_REQUEST': {
+        draft.loading = true;
+        break;
+      }
       case '@stock/STOCK_SUCCESS': {
         draft.stock = action.payload.stock;
+        draft.loading = false;
         break;
       }
       case '@stock/STOCK_FAILURE': {
+        draft.loading = false;
         break;
       }
       default:
